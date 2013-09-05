4fda05b0462b090ba0e0d0c9dd0a9182a4e38c53
Jeremie Koenig <jeremie.koenig@yale.edu>
2013-09-05 19:42:01 -0400

    AbstractData: let the user specify injection relations

--- certikos/AbstractData.v
+++ certikos/AbstractData.v
@@ -1,7 +1,11 @@
+Require Import Coq.Classes.Morphisms.
+Require Import Coq.Classes.RelationPairs.
+Require Import Coq.Program.Basics.
 Require Import AST.
 Require Import Memory.
 Require Import Globalenvs.
 Require Export Functor.
+Require Export Lens.
 Require Import LiftMem.
 
 (** * Types of abstract data *)
@@ -21,10 +25,50 @@ Class AbstractData (data: Type) `{data_ops: AbstractDataOps data} := {
   empty_data_invariant_holds: data_invariant empty_data *)
 }.
 
+Class AbstractDataInjectOps (sdata tdata: Type)
+  `{sdata_ops: AbstractDataOps sdata}
+  `{tdata_ops: AbstractDataOps tdata} :=
+{
+  data_inject: sdata -> tdata -> Prop
+}.
+
+Class AbstractDataInjections (sdata tdata: Type)
+  `{data_inj_ops: AbstractDataInjectOps sdata tdata} :=
+{
+}.
+
+Class AbstractDataHomogenousInjections data
+  `{data_ops: AbstractDataOps data}
+  `{data_inj_ops: !AbstractDataInjectOps data data} :=
+{
+  data_inject_reflexive :> Reflexive data_inject
+}.
+
+
+(** * Default instances for homogenous injections *)
+
+(* Homogenous injections are used when compiling. In this case,
+  we want the abstract data for the source and target to be identical.
+  Therefore we should simply use [eq] for [data_inject]. *)
+
+Section DEFAULTHINJ.
+  Context `{Hdata: AbstractData}.
+
+  Global Instance data_eq_inj_ops: AbstractDataInjectOps data data | 10 :=
+  {
+    data_inject := eq
+  }.
+
+  Global Instance data_eq_inj_spec:
+    AbstractDataInjections data data | 10 := {}.
+
+  Global Instance data_eq_hinj_spec:
+    AbstractDataHomogenousInjections data | 10 := {}.
+End DEFAULTHINJ.
 
 (** * [mem × data] can be used as memory states *)
 
-Section WITHDATA.
+Section LIFTMEM.
   Context mem `{Hmem: Mem.MemoryStates mem}.
   Context data `{data_ops: AbstractData data}.
 
@@ -32,8 +76,53 @@ Section WITHDATA.
     liftmem_empty := (Mem.empty, empty_data)
   }.
 
-  Global Instance data_liftmem: LiftMem (mem × data) mem := {}.
+  Global Instance data_liftmem_spec: LiftMemorySpec (mem × data) mem := {}.
   Proof.
     reflexivity.
   Qed.
-End WITHDATA.
+End LIFTMEM.
+
+Section LIFTINJ.
+  Context `{Hmem_inj: Mem.MemoryInjections}.
+  Context `{Hdata_inj: AbstractDataInjections}.
+
+  Global Instance data_liftinj_ops:
+    LiftInjectOps (smem × sdata) smem (tmem × tdata) tmem :=
+  {
+    liftmem_context_inject s t := data_inject (get s) (get t)
+  }.
+
+  Global Instance data_liftinj_spec:
+    LiftInjections (smem × sdata) smem (tmem × tdata) tmem := {}.
+  Proof.
+    intros [sm1 sd1] [sm2 sd2] Hs [tm1 td1] [tm2 td2] Ht H.
+    unfold same_context in *.
+    simpl in *.
+    specialize (Hs sm1); inversion Hs.
+    specialize (Ht tm1); inversion Ht.
+    congruence.
+  Qed.
+End LIFTINJ.
+
+Section LIFTHINJ.
+  Context `{Hmem_hinj: Mem.HomogenousInjections}.
+  Context `{Hdata_hinj: AbstractDataHomogenousInjections}.
+
+  (* Into Lens.v? *)
+  Instance get_measure `{Getter}: Measure get.
+
+  Global Instance data_lifthinj_spec:
+    LiftHomogenousInjections (mem × data) mem := {}.
+End LIFTHINJ.
+
+(** This should be enough to ensure that instances of [Mem.MemoryStates]
+  can be combined with instances of [AbstractDataHomogenousInjections]
+  to derive [Mem.MemoryStates] instances on the product (mem × data).
+  Below we just make sure that it is indeed the case. *)
+
+Section TEST.
+  Context `{Hmem: Mem.MemoryStates}.
+  Context `{Hdata: AbstractDataHomogenousInjections}.
+
+  Instance: Mem.MemoryStates (mem × data) := _.
+End TEST.
